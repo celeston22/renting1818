28c95f785f63fc4b24ff5f8b32d21bfd8c0b6e38
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-17 15:56:00 +0100

    Cloudinary set-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
   has_many :bookings
   validates :first_name, :last_name, :address_line_1, :city, :zipcode, :country, presence: true
   validates :email, presence: true, uniqueness: true
-end
 
+  # 17.11.2020 Emile : Possibilité d'avoir un avatar par utilisateur
+  has_one_attached :avatar
+end
