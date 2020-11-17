6d749129680014be610ff5a9040c846f0474de1c
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-17 18:23:21 +0100

    V1 component card modifiée, reste à router un click sur card vers show

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,5 +2,5 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_many :booking
   has_one_attached :image
-  validates :title, :type, :description, :daily_price, :address_line_1, :zipcode, :city, :country, :enable, presence: true
+  validates :title, :bike_type, :description, :daily_price, :address_line_1, :zipcode, :city, :country, :enable, presence: true
 end
