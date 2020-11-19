7c400168cfa9907dc42bb0af5bb0d91e4f71c06a
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-19 09:16:04 -0300

    booking inserted on a show

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,14 +1,15 @@
 class BikesController < ApplicationController
-   def show
-       @bike = Bike.find(params[:id])
-   end
+ def show
+     @bike = Bike.find(params[:id])
+     @booking = Booking.new
+ end
 
-  def index
+ def index
     if params[:query].present?
       @bikes = Bike.where("title ILIKE ?", "%#{params[:query]}%")
-    else
+  else
       @bikes = Bike.all
-    end
+  end
 
     # 18.11.2020 the `geocoded` scope filters only bikes with coordinates (latitude & longitude)
     @markers = @bikes.geocoded.map do |bike|
@@ -16,8 +17,8 @@ class BikesController < ApplicationController
         lat: bike.latitude,
         lng: bike.longitude,
         infoWindow: render_to_string(partial: "info_window", locals: { bike: bike })
-      }
-    end
-  end
+    }
+end
+end
 
 end
--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,4 +6,22 @@ class BookingsController < ApplicationController
         # @user = Users.find(params[:user_id])
         @bike = Bike.find(params[:bike_id])
     end
+
+    def create
+        @bike = Bike.find(params[:bike_id])
+        @user = current_user
+        @booking = Booking.new(booking_params)
+        @booking.bike = @bike
+        @booking.user = @user
+        @booking.total_price = @bike.daily_price * (@booking.end_date - @booking.start_date).to_i
+        @booking.status = "pending"
+        @booking.save!
+        redirect_to bike_path(@bike)
+    end
+
+    private
+
+    def booking_params
+        params.require(:booking).permit( :start_date, :end_date )
+    end
 end
