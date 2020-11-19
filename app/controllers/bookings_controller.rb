4a4915d09817f6c3ee150c92481cd9743c9dbc65
jonathan VALERE <jonathan.valere@gmail.com>
2020-11-19 05:19:43 -0300

    trying to fix booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,7 +3,7 @@ class BookingsController < ApplicationController
 
     def new
         @booking = Booking.new
-        # @user = Users.find(params[:id])
+        # @user = Users.find(params[:user_id])
         @bike = Bike.find(params[:bike_id])
     end
 end
